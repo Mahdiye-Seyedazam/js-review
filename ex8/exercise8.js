f59ac1925e5d2822fca75401a509e9d189c18cbd
Mahdiye-Seyedazam <160283210+Mahdiye-Seyedazam@users.noreply.github.com>
2024-02-18 23:25:46 +0330

    Add files via upload

---
+++ ex8/exercise8.js
@@ -0,0 +1,23 @@
+var average = +prompt('enter average of your numbers : ')
+
+switch(average){
+
+    case 18 :
+    case 19 :
+    case 20 :
+        alert("A");
+        break;
+    case 15:
+    case 16:
+    case 17:
+        alert("B");
+        break;
+    case 12:
+    case 13:
+    case 14:
+        alert("C");
+        break;
+    default:
+        alert("out of range!!!");                               
+
+}
