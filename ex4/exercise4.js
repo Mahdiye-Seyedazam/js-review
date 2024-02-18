155c447fd052d7209631633aae40ebf07f0832dc
Mahdiye-Seyedazam <160283210+Mahdiye-Seyedazam@users.noreply.github.com>
2024-02-18 23:23:54 +0330

    Add files via upload

---
+++ ex4/exercise4.js
@@ -0,0 +1,10 @@
+var age = +prompt('enter your age : ', 0)
+
+if(age >= 18){
+
+    alert("your age is ok , you can enter the site!")
+
+}else{
+
+    alert("your age is not ok , you can not enter the site!")
+}
