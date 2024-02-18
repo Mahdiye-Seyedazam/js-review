df4adea0f6a00b40cf0693b7cc1685718cfdc816
Mahdiye-Seyedazam <160283210+Mahdiye-Seyedazam@users.noreply.github.com>
2024-02-18 23:14:29 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var num1 = +prompt("enter your number: ",0)
+var num2 = +prompt("enter your number: ",0)
+var num3 = +prompt("enter your number: ",0)
+
+var average = (num1 + num2 + num3 ) / 3
+
+alert("average of your numbers is : "+average)
