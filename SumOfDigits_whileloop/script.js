1d2ebf6c42c25537d8faf07286c36b88efd881aa
Mahdiye-Seyedazam <160283210+Mahdiye-Seyedazam@users.noreply.github.com>
2024-02-20 23:35:07 +0330

    Add files via upload

---
+++ SumOfDigits_whileloop/script.js
@@ -0,0 +1,31 @@
+var number = Number(prompt('Enter your number : '))
+var counter = 0
+var sum = 0
+
+while(number/10 != 0){
+
+    var yekan = number % 10
+    var sum = sum + yekan
+    var number = Math.floor(number/10)
+    
+
+}
+
+alert(sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
