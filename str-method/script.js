141319ee0566d53d809ada8cb16df2b6f7b24735
Mahdiye-Seyedazam <160283210+Mahdiye-Seyedazam@users.noreply.github.com>
2024-02-20 23:34:34 +0330

    Add files via upload

---
+++ str-method/script.js
@@ -0,0 +1,16 @@
+var userName = prompt("Enter your userNAme : ")
+if (userName == "Ali" || userName == "ALi" || userName == "ALI" || userName == "AlI" || userName == "aLI" || userName == "alI"){
+
+    var lowerUserName = userName.toLowerCase()
+}
+
+if(userName == "ali" || lowerUserName == "ali"){
+
+    alert('you entered in succesfully')
+
+
+}else{
+
+    alert('UserName OR password is not available!')
+}
+
