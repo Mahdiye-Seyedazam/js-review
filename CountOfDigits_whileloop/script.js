5e8a3de6159754acd4ca6cab0786608a2ca4baaf
Mahdiye-Seyedazam <160283210+Mahdiye-Seyedazam@users.noreply.github.com>
2024-02-20 23:30:37 +0330

    Add files via upload

---
+++ CountOfDigits_whileloop/script.js
@@ -0,0 +1,27 @@
+var number = Number(prompt('Enter your number : '))
+var Count = 0
+
+while(number / 10 != 0){
+    
+    
+    number = Math.floor(number/10)
+    Count++
+
+}
+
+
+alert(Count)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
