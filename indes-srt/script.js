4d4eb84503d69a9a0916c46e38d88c415ce5e5cd
Mahdiye-Seyedazam <160283210+Mahdiye-Seyedazam@users.noreply.github.com>
2024-02-20 23:34:12 +0330

    Add files via upload

---
+++ indes-srt/script.js
@@ -0,0 +1,13 @@
+var userName = prompt("Enter your userNAme : ")
+var passWord = prompt("Enter your password : ")
+
+if((userName.length)<3 || (passWord.length)<8 ){
+
+    alert('UserName OR password is not available!')
+
+
+}else{
+
+    alert('you entered in succesfully')
+}
+
