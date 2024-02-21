b11c192855c51a760c0d8fb9a154c59d4c846468
Mahdiye-Seyedazam <160283210+Mahdiye-Seyedazam@users.noreply.github.com>
2024-02-21 23:22:20 +0330

    Add files via upload

---
+++ PowerWithLoop2-/script.js
@@ -0,0 +1,14 @@
+var total =1
+var baseOf = +prompt('What is your base ? ')
+var power = +prompt('What is your Power ? ')
+
+
+while(power>0){
+
+    
+    total = total * baseOf
+    power--
+
+}
+
+alert('total is : '+ total)
