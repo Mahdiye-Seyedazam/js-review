c67e2b955cef85395952c2ece8fba71c2295bdfc
Mahdiye-Seyedazam <160283210+Mahdiye-Seyedazam@users.noreply.github.com>
2024-02-25 22:06:42 +0330

    Add files via upload

---
+++ TaskManagment-d10/script.js
@@ -0,0 +1,14 @@
+var array = {
+    ali : ['html','css','js'],
+    nima : ['next','react','iot'],
+    reza : ['python','pycraft','scrach'],
+    amin : ['php','jango','bootstrap']
+
+}
+
+var taskName = prompt(" what do you want to add ? ")
+
+var employName = prompt(" who do you want to do this ? ")
+
+console.log(array[who].push(menu));
+console.log(array);
